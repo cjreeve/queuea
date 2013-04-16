5f3e1d0ebff23ff1d3bc21b566525bd2c67b9af3
Christopher Reeve <cjreeve@gmail.com>
2013-04-16 12:38:53 +0100

    a new beginning using a fresh install of RVM, ruby 1.9.3 and rails 3.2.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Queuea::Application.config.secret_token = 'c9bb43c736e7fabfae397927fb94d9bfba7c1f531e9a2eeb44b9d0a576b20aa2585b729f2e4abf58522229fe4de4c6556758eed43a6d13929a88614d90eb35ab'
