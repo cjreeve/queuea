65f4a868daa35dbe2c02296d78e243358896d4a7
Christopher Reeve <cjreeve@gmail.com>
2013-04-18 15:48:59 +0100

    attempt to get username working and add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Queuea::Application.routes.draw do
 
+  get "home/index"
+
   devise_for :users
 
-  #root :to => "home#index"
+  root :to => "home#index"
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
